6bf2868940c338ba9e4bb419cb06f29841988050
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2024-11-07 12:24:40 +0100

    Move bootctl to its own source subdirectory
    It's been split into a bunch of files and deserves its own subdirectory
    similarly to systemctl.

--- bootctl-install.h
+++
@@ -1,6 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
-#pragma once
-
-int verb_install(int argc, char *argv[], void *userdata);
-int verb_remove(int argc, char *argv[], void *userdata);
-int verb_is_installed(int argc, char *argv[], void *userdata);
